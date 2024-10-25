c6cf9c0deb4bbd2311c444c32eb4c119bb265ace
JoaoGuiMB <juaogui159@gmail.com>
2024-10-25 19:52:55 -0300

    test: add fake category builder

--- src/category/domain/category.entity.ts
+++ src/category/domain/category.entity.ts
@@ -2,6 +2,7 @@ import { Entity } from "../../shared/domain/entity";
 import { Notification } from "../../shared/domain/validators/notification";
 import { EntityValidationError } from "../../shared/domain/validators/validation.error";
 import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+import { CategoryFakeBuilder } from "./category-fake.builder";
 import { CategoryValidatorFactory } from "./category.validator";
 
 export type CategoryConstructorProps = {
@@ -73,6 +74,10 @@ export class Category extends Entity {
     if (!isValid) {
       throw new EntityValidationError(validator.errorsProps);
     }
+  }
+
+  static fake() {
+    return CategoryFakeBuilder;
   }
 
   toJSON() {
