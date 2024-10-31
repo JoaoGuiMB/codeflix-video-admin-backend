16b55dca41ec40f737d49bdbe7fdf24a5a1d350d
JoaoGuiMB <juaogui159@gmail.com>
2024-10-31 20:11:05 -0300

    test: get category use case integration

---
+++ src/category/application/use-cases/__tests__/integration/get-category.usecase.int.test.ts
@@ -0,0 +1,36 @@
+import { NotFoundError } from "../../../../../shared/domain/errors/not-found.error";
+import { Uuid } from "../../../../../shared/domain/value-objects/uuid.vo";
+
+import { Category } from "../../../../domain/category.entity";
+import { CategoryDrizzleRepository } from "../../../../infra/db/drizzle/category-drizzle.repository";
+
+import { GetCategoryUseCase } from "../../get-category.use-case";
+
+describe("GetCategoryUseCase Integration Tests", () => {
+  let useCase: GetCategoryUseCase;
+  let repository: CategoryDrizzleRepository;
+
+  beforeEach(() => {
+    repository = new CategoryDrizzleRepository();
+    useCase = new GetCategoryUseCase(repository);
+  });
+
+  it("should throws error when entity not found", async () => {
+    const uuid = new Uuid();
+    await expect(() => useCase.execute({ id: uuid.id })).rejects.toThrow(
+      new NotFoundError(uuid.id, Category)
+    );
+  });
+
+  it("should returns a category", async () => {
+    const category = Category.fake().aCategory().build();
+    await repository.insert(category);
+    const output = await useCase.execute({ id: category.category_id.id });
+
+    expect(output.id).toEqual(category.category_id.id);
+    expect(output.name).toEqual(category.name);
+    expect(output.description).toEqual(category.description);
+    expect(output.is_active).toEqual(category.is_active);
+    expect(new Date(output.created_at)).toBeInstanceOf(Date);
+  });
+});
