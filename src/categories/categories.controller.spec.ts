1f04b2bbe144353e3e61052ef2d874ceaf1ba117
JoaoGuiMB <juaogui159@gmail.com>
2024-11-03 11:58:43 -0300

    feat: create category module

---
+++ src/categories/categories.controller.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CategoriesController } from './categories.controller';
+import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
+import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
+import { getModelToken, SequelizeModule } from '@nestjs/sequelize';
+import { DatabaseModule } from 'src/database/database.module';
+import { CategoriesModule } from './categories.module';
+
+describe('CategoriesController', () => {
+  let controller: CategoriesController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [DatabaseModule, CategoriesModule],
+    }).compile();
+
+    controller = module.get<CategoriesController>(CategoriesController);
+  });
+
+  it('should be defined', () => {
+    console.log(controller);
+    expect(controller).toBeDefined();
+  });
+});
