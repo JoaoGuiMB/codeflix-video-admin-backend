3fe9c4ed5488b1a0d9048df4f9766fd34e1c0188
JoaoGuiMB <juaogui159@gmail.com>
2024-11-07 20:21:49 -0300

    feat: error filter

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { WrapperDataInterceptor } from './nest-modules/shared-module/intercerptors/wrapper-data/wrapper-data.interceptor';
+import { NotFoundFilter } from './nest-modules/shared-module/filters/not-found/not-found.filter';
+import { EntityValidationErrorFilter } from './nest-modules/shared-module/filters/not-found/entity-validation-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,5 +16,6 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalInterceptors(new WrapperDataInterceptor());
+  app.useGlobalFilters(new NotFoundFilter(), new EntityValidationErrorFilter());
 }
 bootstrap();
