56999de84aad2ca8fec70902fd83df0eb26a1486
JoaoGuiMB <juaogui159@gmail.com>
2024-11-23 15:59:19 -0300

    feat: thumbnail vo

---
+++ src/core/video/domain/thumbnail-half.vo.ts
@@ -0,0 +1,38 @@
+import { MediaFileValidator } from '@core/shared/domain/validators/media-file.validator';
+import { ImageMedia } from './image-media.vo';
+import { Either } from '@core/shared/domain/either';
+import { VideoId } from './video.aggregate';
+
+export class ThumbnailHalf extends ImageMedia {
+  static max_size = 1024 * 1024 * 2;
+  static mime_types = ['image/jpeg', 'image/png', 'image/gif'];
+
+  static createFromFile({
+    raw_name,
+    mime_type,
+    size,
+    video_id,
+  }: {
+    raw_name: string;
+    mime_type: string;
+    size: number;
+    video_id: VideoId;
+  }) {
+    const mediaFileValidator = new MediaFileValidator(
+      ThumbnailHalf.max_size,
+      ThumbnailHalf.mime_types,
+    );
+
+    return Either.safe(() => {
+      const { name: newName } = mediaFileValidator.validate({
+        raw_name: raw_name,
+        mime_type,
+        size,
+      });
+      return new ThumbnailHalf({
+        name: newName,
+        location: `videos/${video_id.id}/imagens`,
+      });
+    });
+  }
+}
