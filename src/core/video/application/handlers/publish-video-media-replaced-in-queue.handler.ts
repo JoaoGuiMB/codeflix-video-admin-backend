96ecbb1e930bde9d3c47300d6ef46dd526f1da7a
JoaoGuiMB <juaogui159@gmail.com>
2024-12-08 10:49:49 -0300

    test: rabbitmq

--- src/core/video/application/handlers/publish-video-media-replaced-in-queue.handler.ts
+++ src/core/video/application/handlers/publish-video-media-replaced-in-queue.handler.ts
@@ -6,9 +6,7 @@ import { OnEvent } from '@nestjs/event-emitter';
 export class PublishVideoMediaReplacedInQueueHandler
   implements IIntegrationEventHandler
 {
-  constructor(private messageBroker: IMessageBroker) {
-    console.log(messageBroker);
-  }
+  constructor(private messageBroker: IMessageBroker) {}
 
   @OnEvent(VideoAudioMediaUploadedIntegrationEvent.name)
   async handle(event: VideoAudioMediaUploadedIntegrationEvent) {
