5e123d32610f506cb90467cc0589cbea1f160161
JoaoGuiMB <juaogui159@gmail.com>
2024-12-17 19:38:13 -0300

    feat: adding elastic stack

--- src/nest-modules/categories-module/categories.controller.ts
+++ src/nest-modules/categories-module/categories.controller.ts
@@ -54,7 +54,9 @@ export class CategoriesController {
 
   @Get()
   async search(@Query() searchParamsDto: SearchCategoriesDto) {
+    console.log('[fullcycle:init] xpto');
     const output = await this.listUseCase.execute(searchParamsDto);
+    console.log('[fullcycle:finish] xpto');
     return new CategoryCollectionPresenter(output);
   }
 
